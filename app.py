efca055534926862dd96e9807f05645797de27c7
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2022-12-23 10:28:51 -0600

    se añadió el url

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+import requests
 
 apikey="d28508c99b13258f304fcaa2db3b34e3eaed9cb7b3e0f0669b3221cb4701cbdb"
-url="https://apiv3.apifootball.com/?action=get_players&player_name=Sergio Camus&APIkey="
+jugador='Sergio_Camus'
 
-url=url+apikey
+url=
