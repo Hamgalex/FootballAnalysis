dc6b77c852e24e8e185e23a20e9140a581458318
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2022-12-23 18:09:07 -0600

    se hizo el primer request con el número de países

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 import requests
 
-apikey="d28508c99b13258f304fcaa2db3b34e3eaed9cb7b3e0f0669b3221cb4701cbdb"
-jugador='Sergio_Camus'
 
-url=
+apikey="d28508c99b13258f304fcaa2db3b34e3eaed9cb7b3e0f0669b3221cb4701cbdb"
+
+
+urlpaises="https://apiv3.apifootball.com/?action=get_countries&APIkey="+apikey
+
+
+response=requests.get(urlpaises)
+st.write(response.json())
